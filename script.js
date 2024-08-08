8c1d936fc139dbbab52b635c46a01c538881059d
hipopridwan <ridwanhipop@gmail.com>
2024-08-08 17:14:19 +0600

    Roll Dice Event work added

--- script.js
+++ script.js
@@ -11,3 +11,30 @@ const dice = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+let activePlayer = 0;
+let currentScore = 0;
+
+dice.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  const randomDice = Math.trunc(Math.random() * 6) + 1;
+  console.log(randomDice);
+  dice.classList.remove('hidden');
+  dice.src = `dice-${randomDice}.png`;
+  if (randomDice !== 1) {
+    currentScore += randomDice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    document.querySelector(`.player--${activePlayer}`);
+  } else {
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    document.querySelector(`.player--${activePlayer}`);
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+});
